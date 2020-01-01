170a480ca885e17b0609829e663f1e97d6fe0ce5
Tanya Bihag <tbihag@gmail.com>
2020-01-01 20:44:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song 
 attr_accessor :name, :artist, :genre 
 @@count = 0
+@@artist = []
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
+  @@artist << @artist
   @genra = genre 
   @@count += 1 
 end
@@ -13,4 +15,9 @@ def self.count
   @@count
 end
 
+def self.artists(array)
+  artists.each do |x| 
+  
+  
+  
 end
